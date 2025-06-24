4ce86b2ae9b6803dccbd7bbb1b71772d018a056c
sevler <sevler.u17@gmail.com>
2025-06-24 20:50:31 +0300

    Исправление ошибок вывода в консоль, объявления функций

--- sstool/main.cpp
+++ sstool/main.cpp
@@ -7,10 +7,14 @@
 #include <vector>
 #include <string_view>
 #include <conio.h>
+#include <io.h>
+#include <fcntl.h>
 
+void CheckProcessesByName(const std::string& processName);
+bool CheckSuspiciousDLLs(DWORD processID);
 std::vector<void*> pattern_scan(HANDLE hProcess, const std::vector<std::string_view>& patterns);
 
-std::vector<std::string_view> novaPatterns = {
+std::vector<std::string_view> dllPatterns = {
     "font.ttf",
     "Hitboxes.class",
     "canPlaceCrystalServer",
@@ -27,19 +31,33 @@ std::vector<std::string_view> novaPatterns = {
 	"vape"
 };
 
+std::vector<std::string_view> memPatterns = {
+    "font.ttf",
+    "Hitboxes.class",
+    "canPlaceCrystalServer",
+    "FontRenderer"
+    "Retotem",
+    "yIQDgFEROJr",
+    "runecraft"
+    "fastCrystal"
+};
+
 int main() {
-    std::cout << " SinusSMPFinder | MADE BY luvvllx & Sevler v1.3\n";
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    std::cout << " SinusSMPFinder | Создан luvvllx, дополнен Sevler | v1.3" << std::endl;
 
 	// Проверяем все процессы javaw.exe на наличие подозрительных данных
-    FindProcessByName("javaw.exe");
+    CheckProcessesByName("javaw.exe");
 
-    std::cout << "\nНажмите любую клавишу для выхода...\n";
+    std::cout << std::endl << "Нажмите любую клавишу для выхода..." << std::endl;
     _getch();
 
 	return 0;
 }
 
-void FindProcessByName(const std::string& processName) {
+void CheckProcessesByName(const std::string& processName) {
 	PROCESSENTRY32 pe32;
 	pe32.dwSize = sizeof(PROCESSENTRY32);
 	HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
@@ -48,14 +66,32 @@ void FindProcessByName(const std::string& processName) {
 		do {
 			if (std::string(pe32.szExeFile) == processName) {
 				std::cout << "Найден процесс " << processName << " (PID: " << pe32.th32ProcessID << ")" << std::endl;
+				bool found;
+
+				std::cout << "Поиск DLL" << std::endl;
 
 				// Новая проверка по именам загруженных DLL
-				CheckSuspiciousDLLs(pe32.th32ProcessId);
+				found = CheckSuspiciousDLLs(pe32.th32ProcessID);
+
+				std::cout << "Поиск в памяти" << std::endl;
 
 				// Старая проверка по строкам в памяти
-				auto results = pattern_scan(pe32.th32ProcessId, novaPatterns);
+				HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pe32.th32ProcessID);
+                if (hProcess != NULL) {
+                    auto results = pattern_scan(hProcess, memPatterns);
+                    std::cout << "Найдено " << std::dec << results.size() << " Читов" << std::endl;
+                    CloseHandle(hProcess);
 
-        		std::cout << "Найдено " << std::dec << results.size() << " Читов\n";
+                    found = found && results.size() > 0;
+                }
+
+                std::string foundStr = "найдены читы";
+
+                if (!found) {
+                    foundStr = "не " + foundStr;
+                }
+
+                std::cout << "В процессе " << processName << " (PID: " << pe32.th32ProcessID << ") " << foundStr << std::endl;
 			}
 		} while (Process32Next(hSnapshot, &pe32));
 
@@ -63,16 +99,17 @@ void FindProcessByName(const std::string& processName) {
 	}
 }
 
-void CheckSuspiciousDLLs(DWORD processID) {
+bool CheckSuspiciousDLLs(DWORD processID) {
 	HMODULE hMods[1024];
 	HANDLE hProcess;
 	DWORD cbNeeded;
 	char szModName[MAX_PATH];
+	bool found = FALSE;
 
 	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
 
 	if (hProcess == NULL) {
-		return;
+		return FALSE;
 	}
 
 	if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
@@ -80,9 +117,10 @@ void CheckSuspiciousDLLs(DWORD processID) {
 			if (GetModuleFileNameExA(hProcess, hMods[i], szModName, sizeof(szModName))) {
 				std::string dllName(szModName);
 
-				for (const auto& keyword : novaPatterns) {
+				for (const auto& keyword : dllPatterns) {
 					if (dllName.find(keyword) != std::string::npos) {
 						std::cout << "[!] Подозрительная DLL: " << dllName << std::endl;
+						found = TRUE;
 					}
 				}
 			}
@@ -90,10 +128,11 @@ void CheckSuspiciousDLLs(DWORD processID) {
 	}
 
 	CloseHandle(hProcess);
+
+	return found;
 }
 
-std::vector<void*> pattern_scan(DWORD processId, const std::vector<std::string_view>& patterns) {
-	HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, processID);
+std::vector<void*> pattern_scan(HANDLE hProcess, const std::vector<std::string_view>& patterns) {
     SYSTEM_INFO sys_info;
     GetSystemInfo(&sys_info);
 
@@ -114,7 +153,7 @@ std::vector<void*> pattern_scan(DWORD processId, const std::vector<std::string_v
                     size_t pos = 0;
                     while ((pos = view.find(pattern, pos)) != std::string_view::npos) {
                         void* found = static_cast<uint8_t*>(memInfo.BaseAddress) + pos;
-                        std::cout << "[*] FOUND HACKS " << pattern << " at " << std::hex << std::uppercase << reinterpret_cast<uintptr_t>(found) << "\n";
+                        std::cout << "[*] НАЙДЕНО ЧИТОВ " << pattern << " в " << std::hex << std::uppercase << reinterpret_cast<uintptr_t>(found) << std::endl;
                         results.push_back(found);
                         ++pos;
                     }
