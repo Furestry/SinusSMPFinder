cff6a86e45f032ca2e3a0cdc755c3ce969b01f8c
sevler <sevler.u17@gmail.com>
2025-06-25 00:33:25 +0300

    Убрана проверка из-за ненужности

--- sstool/main.cpp
+++ sstool/main.cpp
@@ -14,23 +14,6 @@ void CheckProcessesByName(const std::string& processName);
 bool CheckSuspiciousDLLs(DWORD processID);
 std::vector<void*> pattern_scan(HANDLE hProcess, const std::vector<std::string_view>& patterns);
 
-std::vector<std::string_view> dllPatterns = {
-    "font.ttf",
-    "Hitboxes.class",
-    "canPlaceCrystalServer",
-    "FontRenderer"
-    "Retotem",
-    "yIQDgFEROJr",
-    "runecraft"
-    "fastCrystal",
-	"cheat",
-	"hack",
-	"inject",
-	"xray",
-	"wurst",
-	"vape"
-};
-
 std::vector<std::string_view> memPatterns = {
     "font.ttf",
     "Hitboxes.class",
@@ -68,14 +51,8 @@ void CheckProcessesByName(const std::string& processName) {
 				std::cout << "Найден процесс " << processName << " (PID: " << pe32.th32ProcessID << ")" << std::endl;
 				bool found;
 
-				std::cout << "Поиск DLL" << std::endl;
-
-				// Новая проверка по именам загруженных DLL
-				found = CheckSuspiciousDLLs(pe32.th32ProcessID);
-
 				std::cout << "Поиск в памяти" << std::endl;
 
-				// Старая проверка по строкам в памяти
 				HANDLE hProcess = OpenProcess(PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, pe32.th32ProcessID);
                 if (hProcess != NULL) {
                     auto results = pattern_scan(hProcess, memPatterns);
@@ -97,39 +74,6 @@ void CheckProcessesByName(const std::string& processName) {
 
 		CloseHandle(hSnapshot);
 	}
-}
-
-bool CheckSuspiciousDLLs(DWORD processID) {
-	HMODULE hMods[1024];
-	HANDLE hProcess;
-	DWORD cbNeeded;
-	char szModName[MAX_PATH];
-	bool found = FALSE;
-
-	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
-
-	if (hProcess == NULL) {
-		return FALSE;
-	}
-
-	if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded)) {
-		for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++) {
-			if (GetModuleFileNameExA(hProcess, hMods[i], szModName, sizeof(szModName))) {
-				std::string dllName(szModName);
-
-				for (const auto& keyword : dllPatterns) {
-					if (dllName.find(keyword) != std::string::npos) {
-						std::cout << "[!] Подозрительная DLL: " << dllName << std::endl;
-						found = TRUE;
-					}
-				}
-			}
-		}
-	}
-
-	CloseHandle(hProcess);
-
-	return found;
 }
 
 std::vector<void*> pattern_scan(HANDLE hProcess, const std::vector<std::string_view>& patterns) {
